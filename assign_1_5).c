68793a78f1b69c379bd7e2adb9fabe6d9bd417bc
Shreyash Desai <shreyasdesai96k@gmail.com>
2025-12-13 09:45:59 +0530

    assign-1

---
+++ assign_1_5).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char b;
+    int h;
+    int p;
+    int res=0;
+    
+    printf("enter a charachter : ");
+    scanf("\n %c",&b);
+    
+    
+  
+    
+    p=b;
+    printf("ascii value",b);
+    
+    res= p^32;
+printf(" ascii  %d  ",res);
+
+printf(" \nresult  %c  ",res);
+    
+}
